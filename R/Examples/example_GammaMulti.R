ec46d46ff965befec48c0bc8c07a8d19b9b06caa
gajdosandrej <38565719+gajdosandrej@users.noreply.github.com>
2024-05-24 11:31:41 +0200

    Delete R directory

--- R/Examples/example_GammaMulti.R
+++
@@ -1,5 +0,0 @@
-## EXAMPLE 1
-t <- seq(-10, 10, length.out = 101)
-z <- 1i * t
-p <- 10
-f <- GammaMulti(z, p)
--- R/Examples/example_Hypergeom1F1Mat.R
+++
@@ -1,7 +0,0 @@
-## EXAMPLE
-a <- 3
-b <- 5
-X <- c(1, 2, 3)
-MAX <- 10
-f <- Hypergeom1F1Mat(a, b, X, MAX)
-
